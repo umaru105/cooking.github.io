5d13ec6c309ae97293212aad8bfc8a978b392fc1
umaru105 <43401146+umaru105@users.noreply.github.com>
2018-09-26 15:26:04 +0700

    Add files via upload

---
+++ siskasiti.blade.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Theme Made By www.w3schools.com - No Copyright -->
+  <title>Siska Siti</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+  body {
+      font: 20px Montserrat, sans-serif;
+      line-height: 1.8;
+      color: #f5f6f7;
+  }
+  p {font-size: 16px;}
+  .margin {margin-bottom: 45px;}
+  .bg-1 { 
+      background-image: url("https://previews.123rf.com/images/nitr/nitr1610/nitr161000019/65328027-empty-pan-with-various-products-for-cooking-on-wooden-table-top-view-banner.jpg");
+      height: 400px;
+  }
+  .bg-2 { 
+      padding: 5PX 10PX;
+      background-color:  #B8860B; 
+      color: #ffffff;
+  }
+  .bg-3 { 
+      background-color: #ffffff; 
+      color: #555555;
+  }
+  .bg-4 { 
+      background-color: grey;
+      color: #fff;
+  }
+  .navbar {
+      margin-bottom: 0;
+      background-color: #f4511e;
+      z-index: 9999;
+      border: 0;
+      font-size: 12px !important;
+      line-height: 1.42857143 !important;
+      letter-spacing: 4px;
+      border-radius: 0;
+      font-family: Montserrat, sans-serif;
+  }
+   .navbar li a, .navbar .navbar-brand {
+      color: #fff !important;
+  }
+  .navbar-nav li a:hover, .navbar-nav li.active a {
+      color: #f4511e !important;
+      background-color: #fff !important;
+  }
+  .navbar-default .navbar-toggle {
+      border-color: transparent;
+      color: #fff !important;
+  }
+  
+  .search {
+      background-color: #DEB887 ;
+      color: #fff;
+      padding: 10px 25px;
+      font-family: Montserrat, sans-serif;
+  }
+   .container-fluid {
+      padding: 60px 50px;
+  }
+  
+  </style>
+</head>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#myPage">HOME</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#about">ABOUT</a></li>
+        <li><a href="#categories">CATEGORIES</a></li>
+        <li><a href="#pricing">PRICING</a></li>
+        <li><a href="#contact">CONTACT US</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<!-- First Container -->
+<div class="bg-1 text-center">
+</br>
+</br>
+  <h1 style="font-size: 80px">  Let's Cooking</h1>
+  <h1>With Us</h1>
+</div>
+
+<!-- Second Container -->
+<div class="search text-center">
+  <h1>FREE RECIPES, GUIDES AND MORE</h1> 
+  <p>We Update for Every </p> 
+  <form>
+    <div class="input-group">
+      <input type="text" class="form-control" size="50" placeholder="What would you like to cook?" required>
+      <div class="input-group-btn">
+        <button type="button" class="btn btn-danger">Search</button>
+      </div>
+    </div>
+  </form>
+</div>
+
+
+
+<!-- Container (Services Section) -->
+<div id="categories" class="bg-2 text-center">
+  <h2>CATEGORIES</h2>
+  <h4>What we offer</h4>
+  <br>
+  
+</div>
+
+<!-- Footer -->
+<footer class="bg-4 text-align:center">
+  <p>Copyright@siskasiti 2018</p> 
+</footer>
+
+</body>
+</html>
